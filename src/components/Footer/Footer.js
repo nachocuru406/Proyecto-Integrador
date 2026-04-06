4addc192d44df18eff1024eea6e5696e8c942689
JoaquinFalcao <jfalcao@udesa.edu.ar>
2026-04-06 19:50:28 -0300

    Cree el Header y el Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+ class Footer extends Component {
+  render() {
+    return (
+      <footer className = "alert alert-primary mt-4 text-center">
+         <p className = "mb-0">Joaquin Falcao | Ignacio Curutchet</p>
+      </footer>
+    )
+  }
+}
+
+export default Footer
---
+++ src/components/Header/Header.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react'
+import {Link} from "react-router-dom"
+
+ class Header extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            haySesion: false
+        }
+    }
+    componentDidMount(){
+        let cookies = document.cookie
+        if (cookies.includes("session=")){
+            this.setState({
+                haySesion: true
+            })
+        }
+    }
+    render() {
+    return (
+         <nav>
+            <ul className = "nav nav-tabs my-4">
+                <li className = "nav-item">
+                    <Link className = "nav-link" to = "/home">Home</Link>
+                </li>
+                <li className = "nav-item">
+                    <Link className = "nav-link" to = "/peliculas">Peliculas</Link>
+                </li>
+                <li className = "nav-item">
+                    <Link className = "nav-link" to = "/series">Series</Link>
+                </li>
+                {this.state.haySesion ?
+                <li className = "nav-item">
+                    <Link className = "nav-link" to = "/favoritos">Favoritos</Link>
+                </li>
+                : null}
+                {!this.state.haySesion ? 
+                <li className = "nav-item ml-auto">
+                    <Link className = "nav-link" to = "/registro">Registro</Link>
+                </li>
+                : null}
+                {!this.state.haySesion ?
+                <li className = "nav-item">
+                   <Link className  ="nav-link" to = "/login">Login</Link>
+                </li>
+                : null}
+            </ul>
+        </nav>
+    )
+  }
+}
+
+export default Header
