e754a7c4ea3b05f8d60f99a1f874f5db4167eb14
nachocuru406 <icurutchet@udesa.edu.ar>
2026-04-14 17:50:47 -0300

    Termine el buscador

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import Home from './Screens/Home/Home';
-
-
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Screens/Home/Home";
+import SearchResults from "./Screens/SearchResults/SearchResults";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className='container'>
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/search/:query" component={SearchResults} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
+
 export default App;
